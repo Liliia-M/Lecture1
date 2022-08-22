1b481e821fadec5bfc8b4323e8927774c7744b18
Liliia-M <smak0991@mail.ru>
2022-08-22 21:26:06 +0300

    task 3

---
+++ seminar2task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+//int result1 = number/
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"{number}->да");
+}
+else
+{
+   Console.WriteLine($"{number}->нет"); 
+}
