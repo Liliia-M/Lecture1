752c3ad2fb409e4d3b139090f138c7882c2735ac
Liliia-M <smak0991@mail.ru>
2022-08-22 20:46:11 +0300

    seminar2 task1

---
+++ seminar2task1/Program.cs
@@ -0,0 +1,9 @@
+﻿int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int a1 = num / 100;
+int a2 = (num/10) % 10;
+int a3 = num % 10;
+int a4 = a1*10;
+int result = a3 + a4;
+
+Console.WriteLine($"{result}");
