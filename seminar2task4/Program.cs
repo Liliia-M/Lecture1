67c219eff3ada1774d9a4c2111fb42494e8c2ad7
Liliia-M <smak0991@mail.ru>
2022-08-22 21:38:51 +0300

    task4

---
+++ seminar2task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int sqr1 = numberA * numberA;
+int sqr2 = numberB * numberB;
+
+if (numberB == sqr1||numberA == sqr2)
+{
+    Console.WriteLine($"{numberA}, {numberB}->да");
+}
+else
+{
+   Console.WriteLine($"{numberA}, {numberB}->нет"); 
+}
