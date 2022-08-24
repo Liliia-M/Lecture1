1acb84e155e1a7fd8851e82df955f9d5705caaa4
Liliia-M <smak0991@mail.ru>
2022-08-24 22:33:28 +0300

    Lecture 2 Example 4

---
+++ Lecture_2_Example_4_ArrayLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+int[] array = new int[10];
+//new int[10] - создай новый массив, в котором будет 10 элементов
+
+FillArray(array);
+PrintArray(array);
