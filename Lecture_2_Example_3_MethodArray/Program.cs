b7f2a55491e24857fa334418738d07050736c58a
Liliia-M <smak0991@mail.ru>
2022-08-24 22:01:35 +0300

    Lecture 2 Example 3 Method Array

---
+++ Lecture_2_Example_3_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 1, 12, 31, 4, 18, 16, 17, 18 };
+//              0   1   2  3   4   5   6   7
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;//прерви - чтобы показывал только первое значение find из всех имеющихся
+    }
+        //index = index + 1
+        index++;
+}
+
