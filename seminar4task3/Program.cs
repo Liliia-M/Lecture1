35e18d735974a9a6213f07066ea7f27d87d52bc2
Liliia-M <smak0991@mail.ru>
2022-08-29 21:34:43 +0300

    seminar 4 task 3

---
+++ seminar4task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] RunR()
+{
+int[] rarray = new int[8];
+
+    for (int i = 0; i < rarray.Length; i++)
+    {
+        rarray[i] = new Random().Next(0, 2);
+    }
+    return rarray;
+
+}
+
+
+Console.WriteLine(String.Join(" ", RunR()));
