1363831414ec71d1aa309a7360a4a76b0e92b973
Liliia-M <smak0991@mail.ru>
2022-08-29 21:00:56 +0300

    seminar 4 task 2

---
+++ seminar4task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int GetNum(int num)
+{
+    int equal = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        equal = equal * i;
+    }
+    return equal;
+}
+
+
+
+Console.WriteLine("Ведите число N: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{GetNum(number)}");
